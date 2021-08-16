49e46605db9ee7dfd8cb229c74564e3040d5f921
Taylor <taylorjcochran@hotmail.com>
2021-08-16 13:15:24 -0700

    _build_report tests pass

--- src/tests/collectors/test_otx.py
+++ src/tests/collectors/test_otx.py
@@ -76,3 +76,37 @@ def test_parser_empty_report(parser: OTX_Parser) -> None:
     assert expected_keys == actual_keys
 
 
+#       ======================================
+#           parser._build_report
+#       ======================================
+
+
+def test_parser_build_report_none(parser: OTX_Parser) -> None:
+    raw_report = None
+    actual = parser._build_report(raw_report)
+
+    expected = None
+    assert expected == actual
+
+
+def test_parser_build_report_empty(parser: OTX_Parser) -> None:
+    raw_report = {}
+    actual = parser._build_report(raw_report)
+
+    expected = None
+    assert expected == actual
+
+
+def test_parser_build_report_good(parser: OTX_Parser, raw_report: dict) -> None:
+    actual = parser._build_report(raw_report)
+
+    expected_keys = {
+        "asn",
+        "Country",
+        "City",
+        "Threat Score",
+        "Type"
+    }
+    actual_keys = set(actual.keys())
+    assert expected_keys == actual_keys
+
